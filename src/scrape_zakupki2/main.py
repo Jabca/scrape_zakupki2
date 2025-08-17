bd4f7f54d769097747567661c87f74a6f31c853e
Jabca <pavel.sterlin.work@gmail.com>
2025-08-17 04:51:35 +0300

    local vars for threadpool executors

--- src/scrape_zakupki2/main.py
+++ src/scrape_zakupki2/main.py
@@ -29,13 +29,13 @@ def scrape_zakupki(
             f"step is negative: {timedelta_step} so the search will never finish"
         )
 
-    futures: list[Any] = list()
-
     # divide search date range into small
     with ThreadPoolExecutor(max_workers=pool_size) as exe:
         left = pubdate_from
         right = left + timedelta_step
         right = min(right, pubdate_to)
+
+        futures: list[Any] = list()
 
         while right <= pubdate_to and left < pubdate_to:
             args = load_arguments(
@@ -52,46 +52,48 @@ def scrape_zakupki(
             right += timedelta_step
             right = min(right, pubdate_to)
 
-    tasks: list[load_arguments] = list()
-    # for each date range create ranges with 500 in each (cause stuff works like this)
-    for future in as_completed(futures):
-        num, dates = future.result()
-        if num is None:
-            continue
-        left = 1
-        right = min(500, num)
-        while right <= num and left < num:
-            tasks.append(
-                load_arguments(
-                    search_str=search_str,
-                    pubdate_from=dates[0],
-                    pubdate_to=dates[1],
-                    include_fz223=include_fz223,
-                    include_fz44=include_fz44,
-                    from_=left,
-                    to=right,
+        tasks: list[load_arguments] = list()
+        # for each date range create ranges with 500 in each (cause stuff works like this)
+        for future in as_completed(futures):
+            num, dates = future.result()
+            if num is None:
+                continue
+            left = 1
+            right = min(500, num)
+            while right <= num and left < num:
+                tasks.append(
+                    load_arguments(
+                        search_str=search_str,
+                        pubdate_from=dates[0],
+                        pubdate_to=dates[1],
+                        include_fz223=include_fz223,
+                        include_fz44=include_fz44,
+                        from_=left,
+                        to=right,
+                    )
                 )
-            )
-            left += 500
-            right += 500
-
-    futures.clear()
-    with ThreadPoolExecutor(max_workers=pool_size) as exec:
-        for task in tasks:
-            futures.append(exec.submit(load_csv, task))
+                left += 500
+                right += 500
 
     results: list[pd.DataFrame] = list()
-    for future in as_completed(futures):
-        res = future.result()
-        if res is None:
-            continue
-        results.append(res)
+    with ThreadPoolExecutor(max_workers=pool_size) as exec:
+        futures.clear()
+        for task in tasks:
+            futures.append(exec.submit(load_csv, task))
+        for future in as_completed(futures):
+            res = future.result()
+            if res is None:
+                continue
+            results.append(res)
 
-    return pd.concat(results)
+    if results:
+        return pd.concat(results)
+    else:
+        return None
 
 
 if __name__ == "__main__":
-    res = scrape_zakupki(search_str="бумага", pubdate_from=date(2024, 3, 1))
+    res = scrape_zakupki(search_str="kfjskldjf", pubdate_from=date(2024, 3, 1))
 
     with open("test.csv", mode="w") as f:
         if res is not None:
